53b485384b2ea3c0b0296ba148eedf47f45dc291
Victoria Dobryden <65075884+victoriadobryden@users.noreply.github.com>
2024-02-27 01:53:01 +0200

    Add files via upload

--- src/main/java/SouvenirService.java
+++ src/main/java/SouvenirService.java
@@ -1,13 +1,24 @@
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class SouvenirService {
-     public void addSouvenir(String string) {
+     public static void addSouvenir(HashMap<Integer, Manufacturer> mf, HashMap<Integer, Souvenir> sv, Scanner sc,
+                                    int svId) {
+          ManufacturerService.viewManufacturers(mf);
+          System.out.println("Enter ID of manufacturer, what you want to add souvenir to");
+          int ID = sc.nextInt();
+          System.out.println("Enter name, release date, and price of souvenir");
+          String s = sc.nextLine();
+          sv.put(svId++, new Souvenir(s.split(" ")[0], ID, LocalDate.parse(s.split(" ")[1]),
+                  Double.parseDouble(s.split(" ")[2])));
+     }
+
+     public static void viewManufacturers() {
 
      }
 
-     public void viewManufacturers() {
-
-     }
-
-     public void editManufacturers() {
+     public static void editManufacturers() {
 
      }
 
